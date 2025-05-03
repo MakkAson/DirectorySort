2e40dde40989277c0a4639bd701663f5e09f0482
MakkAson <115122048+MakkAson@users.noreply.github.com>
2025-05-02 23:10:34 -0400

    Update DirectorySort.py
    Fixed infinite nesting issue that could occur if sorted folders already exist

--- DirectorySort.py
+++ DirectorySort.py
@@ -11,6 +11,10 @@ assert Directory.exists(), "Error: Not a valid directory"
 for child in Directory.iterdir():
     ModifiedDate = datetime.date.fromtimestamp(child.stat().st_mtime)
     Folder = Path(f"{Directory}" + r"\\" + f"{ModifiedDate.year}")
-    try: Folder.mkdir()
+    try:
+        Folder.mkdir()
+        child.rename(Folder / child.name)
     except: pass
-    child.rename(Folder / child.name)
+    #If is a file and sorted directory exists, move to it
+    if child.is_file():
+        child.rename(Folder / child.name)
